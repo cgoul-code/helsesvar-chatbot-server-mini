ffdb901a3e197e31fcd254ef81e90721ce73130b
cgoul-code <christian@goulignac.net>
2025-10-10 10:35:50 +0200

    ' fix for relevancy (0.45->0.5)'

--- config.py
+++ config.py
@@ -132,11 +132,10 @@ LLMGPT4 = AzureChatOpenAI(
     api_version=os.getenv('AZURE_OPENAI_API_VERSION'),
     #api_key=os.getenv('AZURE_OPENAI_API_KEY'),
     azure_endpoint=os.getenv('AZURE_OPENAI_ENDPOINT'),
-
-    #temperature=0.0,
     timeout=120,
+    reasoning_effort="minimal",
+    verbose=True
 )
-
 server_settings.set_llm(LLMGPT4)
 
 
